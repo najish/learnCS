81c0b860f3b65b7f1b665f4b49a7dec4f2ca2c84
anonymous <anonymos@gmail.com>
2024-03-15 19:18:52 +0530

    Third Day : Development

--- js/script.js
+++ js/script.js
@@ -7,7 +7,19 @@ const loginform = document.getElementById('login');
 const signupform = document.getElementById('signup');
 const loginbtn = document.querySelector('.login-btn');
 const signupbtn = document.querySelector('.signup-btn');
+const home = document.querySelector('.home');
+const courses = document.querySelector('.courses');
 
+function loadJsonData(url) {
+    fetch(url).then(response => {
+        if(!response.ok) {
+            console.log('status code is not okay');
+        }
+        return response.json();
+    }).then(courses => {
+        console.log(courses);
+    }).catch(err => console.error(err));
+}
 
 function loadHtmlFile(url) {
     fetch(url).then(response => {
@@ -21,6 +33,16 @@ function loadHtmlFile(url) {
         console.error('file not present');
     })
 }
+courses.addEventListener('click', e => {
+    e.preventDefault();
+    console.log('buttton is clicked');
+    loadJsonData('http://localhost:3000/students');
+
+});
+home.addEventListener('click',e => {
+    e.preventDefault();
+    contaniner.innerHTML = '';
+});
 
 loginbtn.addEventListener('click',e => {
     e.preventDefault();
